719b2dfc7b32b7c19d805c9607dd328d033d7e95
GustavoMota03 <gustavomota.work@gmail.com>
2022-03-10 09:45:54 +0000

    first commit

---
+++ src/com/gustavo/core/Main.java
@@ -0,0 +1,8 @@
+package com.gustavo.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Menu.processarMenu();
+    }
+}
---
+++ src/com/gustavo/core/Menu.java
@@ -0,0 +1,9 @@
+package com.gustavo.core;
+
+public class Menu {
+    public static void processarMenu(){
+        System.out.println("Hello World!");
+
+
+    }
+}
